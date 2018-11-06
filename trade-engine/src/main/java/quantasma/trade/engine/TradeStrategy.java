d83bd6427c531d05b3dd9231d0998af05ba7f04a
Piotr Morończyk <p.moronczyk@gmail.com>
2018-11-06 14:16:27 +0100

    Remove should* methods
    Lacks plasticity

--- trade-engine/src/main/java/quantasma/trade/engine/TradeStrategy.java
+++ trade-engine/src/main/java/quantasma/trade/engine/TradeStrategy.java
@@ -3,7 +3,6 @@ package quantasma.trade.engine;
 import org.ta4j.core.BaseStrategy;
 import org.ta4j.core.Rule;
 import org.ta4j.core.Strategy;
-import org.ta4j.core.TradingRecord;
 
 public class TradeStrategy extends BaseStrategy {
     private final Context context;
@@ -30,24 +29,6 @@ public class TradeStrategy extends BaseStrategy {
     @Override
     public Strategy or(String name, Strategy strategy, int unstablePeriod) {
         return new TradeStrategy(context, name, getEntryRule().or(strategy.getEntryRule()), getExitRule().or(strategy.getExitRule()), unstablePeriod);
-    }
-
-    @Override
-    public boolean shouldEnter(int index, TradingRecord tradingRecord) {
-        final boolean shouldEnter = super.shouldEnter(index, tradingRecord);
-        if (shouldEnter) {
-            context.getOrderService().openPosition(null);
-        }
-        return shouldEnter;
-    }
-
-    @Override
-    public boolean shouldExit(int index, TradingRecord tradingRecord) {
-        final boolean shouldExit = super.shouldExit(index, tradingRecord);
-        if (shouldExit) {
-            context.getOrderService().closePosition(null);
-        }
-        return shouldExit;
     }
 
     @Override
