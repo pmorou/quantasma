9b0eb01c3e02a808f5ee1594c4ed05779217c98e
Piotr Morończyk <p.moronczyk@gmail.com>
2018-11-08 20:41:21 +0100

    Comment fix

--- examples/src/main/java/quantasma/examples/TradeAppExample.java
+++ examples/src/main/java/quantasma/examples/TradeAppExample.java
@@ -33,7 +33,7 @@ public class TradeAppExample {
         // Example call on market price change
         tradeEngine.process("EURUSD", ZonedDateTime.now(), 1.13757, 1.13767);
 
-        // Will fail silently as the symbol wasn't registered in time series definition
+        // Will fail silently as the symbol wasn't registered within time series definitions
         tradeEngine.process("EURJPY", ZonedDateTime.now(), 129.653, 129.663);
     }
 }
