369ff53a7e280cf0dc33e25e0117753885298a55
Piotr Morończyk <p.moronczyk@gmail.com>
2018-11-21 15:49:05 +0100

    Remove unused classes

--- quantasma-core/src/main/java/quantasma/core/timeseries/AggregableTimeSeries.java
+++
@@ -1,8 +0,0 @@
-package quantasma.core.timeseries;
-
-import org.ta4j.core.TimeSeries;
-
-public interface AggregableTimeSeries {
-
-    TimeSeries aggregate(TimeSeriesDefinition timeSeriesDefinition);
-}
