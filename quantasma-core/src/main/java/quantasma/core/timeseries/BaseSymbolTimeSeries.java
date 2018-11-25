8f28c08047cc3ffe96f0c1a3f5e28c0c8441f05f
Piotr Morończyk <p.moronczyk@gmail.com>
2018-11-25 15:32:40 +0100

    Create builder

--- quantasma-core/src/main/java/quantasma/core/timeseries/BaseSymbolTimeSeries.java
+++ quantasma-core/src/main/java/quantasma/core/timeseries/BaseSymbolTimeSeries.java
@@ -1,8 +1,16 @@
 package quantasma.core.timeseries;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import org.ta4j.core.Bar;
 import org.ta4j.core.BaseTimeSeries;
+import org.ta4j.core.num.Num;
+import org.ta4j.core.num.PrecisionNum;
 import quantasma.core.BarPeriod;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
 @Getter
 public class BaseSymbolTimeSeries extends BaseTimeSeries implements SymbolTimeSeries {
@@ -20,4 +28,55 @@ public class BaseSymbolTimeSeries extends BaseTimeSeries implements SymbolTimeSe
         this(name, symbol, barPeriod, Integer.MAX_VALUE);
     }
 
+    public BaseSymbolTimeSeries(Builder builder) {
+        super(builder.getName(), builder.getBars(), builder.getNumFunction());
+        this.symbol = builder.getSymbol();
+        this.barPeriod = builder.getBarPeriod();
+        setMaximumBarCount(builder.getMaxBarCount());
+    }
+
+    @Getter(value = AccessLevel.PROTECTED)
+    public static class Builder<T extends Builder<T>> {
+        private final String symbol;
+        private final BarPeriod barPeriod;
+
+        private String name = "unamed_series";
+        private List<Bar> bars = new ArrayList<>();
+        private int maxBarCount = Integer.MAX_VALUE;
+        private Function<Number, ? extends Num> numFunction = PrecisionNum::valueOf;
+
+        public Builder(String symbol, BarPeriod barPeriod) {
+            this.symbol = symbol;
+            this.barPeriod = barPeriod;
+        }
+
+        public T withName(String name) {
+            this.name = name;
+            return self();
+        }
+
+        public T withNumFunction(Function<Number, ? extends Num> numFunction) {
+            this.numFunction = numFunction;
+            return self();
+        }
+
+        public T withBars(List<Bar> bars) {
+            this.bars = bars;
+            return self();
+        }
+
+        public T withMaxBarCount(int maxBarCount) {
+            this.maxBarCount = maxBarCount;
+            return self();
+        }
+
+        protected T self() {
+            return (T) this;
+        }
+
+        public BaseSymbolTimeSeries build() {
+            return new BaseSymbolTimeSeries(this);
+        }
+
+    }
 }
