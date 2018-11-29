aaa2a02b9d3b5ec3db303ed635d72eaa783831cf
Piotr Morończyk <p.moronczyk@gmail.com>
2018-11-29 19:29:24 +0100

    Test root controller

---
+++ quantasma-app/src/test/java/quantasma/app/controller/RootControllerTest.java
@@ -0,0 +1,27 @@
+package quantasma.app.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class RootControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void rootOk() throws Exception {
+        mockMvc.perform(get("test"))
+               .andExpect(status().isOk())
+               .andExpect(content().string("OK"));
+    }
+}
