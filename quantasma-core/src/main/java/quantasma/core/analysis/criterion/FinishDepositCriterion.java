731bda26076ba7e4c91c02195c48217b655a9e00
Piotr Morończyk <p.moronczyk@gmail.com>
2018-12-05 01:29:44 +0100

    Extract profit loss criterion

--- quantasma-core/src/main/java/quantasma/core/analysis/criterion/FinishDepositCriterion.java
+++ quantasma-core/src/main/java/quantasma/core/analysis/criterion/FinishDepositCriterion.java
@@ -1,6 +1,5 @@
 package quantasma.core.analysis.criterion;
 
-import org.ta4j.core.Order;
 import org.ta4j.core.TimeSeries;
 import org.ta4j.core.Trade;
 import org.ta4j.core.TradingRecord;
@@ -10,11 +9,11 @@ import org.ta4j.core.num.Num;
 public class FinishDepositCriterion extends AbstractAnalysisCriterion {
 
     private final double initialDeposit;
-    private final ProfitLossPipsCalculator profitLossPipsCalculator;
+    private final ProfitLossCriterion profitLossPipsCalculator;
 
     public FinishDepositCriterion(double initialDeposit, double pipResolution) {
         this.initialDeposit = initialDeposit;
-        profitLossPipsCalculator = new ProfitLossPipsCalculator(pipResolution);
+        profitLossPipsCalculator = new ProfitLossCriterion(pipResolution);
     }
 
     @Override
@@ -34,47 +33,5 @@ public class FinishDepositCriterion extends AbstractAnalysisCriterion {
         return criterionValue1.isGreaterThan(criterionValue2);
     }
 
-    public static class ProfitLossPipsCalculator {
 
-        final double pipResolution;
-
-        public ProfitLossPipsCalculator(double pipResolution) {
-            this.pipResolution = pipResolution;
-        }
-
-        public Num calculate(TimeSeries series, Trade trade) {
-            if (trade.isClosed()) {
-                final Num exitClosePrice = getPrice(series, trade.getExit());
-                final Num entryClosePrice = getPrice(series, trade.getEntry());
-                final Num pips = difference(trade, exitClosePrice, entryClosePrice)
-                        .dividedBy(series.numOf(pipResolution));
-                return toRealUnit(series, trade, pips);
-            }
-            return series.numOf(0);
-        }
-
-        private Num getPrice(TimeSeries series, Order exit) {
-            return hasPrice(exit) ?
-                    exit.getPrice() : series.getBar(exit.getIndex()).getClosePrice();
-        }
-
-        private Num difference(Trade trade, Num exitClosePrice, Num entryClosePrice) {
-            return trade.getEntry().isBuy() ?
-                    exitClosePrice.minus(entryClosePrice) : entryClosePrice.minus(exitClosePrice);
-        }
-
-        private boolean hasPrice(Order exit) {
-            return !exit.getPrice().isNaN();
-        }
-
-        private final static int STANDARD_LOT_SIZE = 100_000;
-        private final static int STANDARD_LOT_PROFIT = 10;
-
-        private static Num toRealUnit(TimeSeries timeSeries, Trade trade, Num pips) {
-            return trade.getExit().getAmount()
-                        .dividedBy(timeSeries.numOf(STANDARD_LOT_SIZE))
-                        .multipliedBy(timeSeries.numOf(STANDARD_LOT_PROFIT))
-                        .multipliedBy(pips);
-        }
-    }
 }
---
+++ quantasma-core/src/main/java/quantasma/core/analysis/criterion/ProfitLossCriterion.java
@@ -0,0 +1,43 @@
+package quantasma.core.analysis.criterion;
+
+import org.ta4j.core.TimeSeries;
+import org.ta4j.core.Trade;
+import org.ta4j.core.TradingRecord;
+import org.ta4j.core.analysis.criteria.AbstractAnalysisCriterion;
+import org.ta4j.core.num.Num;
+
+public class ProfitLossCriterion extends AbstractAnalysisCriterion {
+    private final static int STANDARD_LOT_SIZE = 100_000;
+    private final static int STANDARD_LOT_PROFIT = 10;
+
+    private final ProfitLossPipsCriterion pipsCriterion;
+
+    public ProfitLossCriterion(double pipResolution) {
+        this.pipsCriterion = new ProfitLossPipsCriterion(pipResolution);
+    }
+
+    @Override
+    public Num calculate(TimeSeries series, Trade trade) {
+        return toRealUnit(series, trade, pipsCriterion.calculate(series, trade));
+    }
+
+    private static Num toRealUnit(TimeSeries timeSeries, Trade trade, Num pips) {
+        return trade.getExit().getAmount()
+                    .dividedBy(timeSeries.numOf(STANDARD_LOT_SIZE))
+                    .multipliedBy(timeSeries.numOf(STANDARD_LOT_PROFIT))
+                    .multipliedBy(pips);
+    }
+
+    @Override
+    public Num calculate(TimeSeries series, TradingRecord tradingRecord) {
+        return tradingRecord.getTrades()
+                            .stream()
+                            .map(trade -> calculate(series, trade))
+                            .reduce(series.numOf(0), Num::plus);
+    }
+
+    @Override
+    public boolean betterThan(Num criterionValue1, Num criterionValue2) {
+        return criterionValue1.isGreaterThan(criterionValue2);
+    }
+}
