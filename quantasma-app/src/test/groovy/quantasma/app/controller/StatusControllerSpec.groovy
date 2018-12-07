2595e40287a104829f5c15e21f645b879c025f62
Piotr Morończyk <p.moronczyk@gmail.com>
2018-12-07 02:35:55 +0100

    To spock

--- quantasma-app/src/test/groovy/quantasma/app/controller/StatusControllerSpec.groovy
+++ quantasma-app/src/test/groovy/quantasma/app/controller/StatusControllerSpec.groovy
@@ -1,25 +1,21 @@
 package quantasma.app.controller
 
-import org.junit.Test
-import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@RunWith(SpringRunner.class)
 @WebMvcTest(StatusController)
-class StatusControllerSpec {
+class StatusControllerSpec extends Specification {
 
     @Autowired
     private MockMvc mockMvc
 
-    @Test
-    void statusOk() throws Exception {
+    def 'status returns OK'() throws Exception {
         mockMvc.perform(get("status"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("OK"))
