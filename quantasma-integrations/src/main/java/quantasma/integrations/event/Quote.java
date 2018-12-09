6ab2e427a436663cfcc12d328e27661219a203fd
Piotr Morończyk <p.moronczyk@gmail.com>
2018-12-09 14:39:37 +0100

    Add time field

--- quantasma-integrations/src/main/java/quantasma/integrations/event/Quote.java
+++ quantasma-integrations/src/main/java/quantasma/integrations/event/Quote.java
@@ -2,9 +2,12 @@ package quantasma.integrations.event;
 
 import lombok.Data;
 
+import java.time.ZonedDateTime;
+
 @Data
 public class Quote {
     private final String symbol;
+    private final ZonedDateTime time;
     private final double bid;
     private final double ask;
 }
