d40d69ffb0f546bf58a9f2b291063588bb1c4937
Piotr Morończyk <p.moronczyk@gmail.com>
2018-12-10 23:34:03 +0100

    Refactor

--- quantasma-app/src/main/resources/frontend/src/app/quotes/quotes.component.ts
+++ quantasma-app/src/main/resources/frontend/src/app/quotes/quotes.component.ts
@@ -12,23 +12,22 @@ export class QuotesComponent implements OnInit {
   constructor(private events: EventsService) { }
 
   ngOnInit() {
-    this.updateCurrency();
+    this.events.quotes().subscribe(this.renderQuote);
   }
 
-  updateCurrency(): void {
-    this.events.quotes().subscribe((value: Quote) => {
-        let li = document.getElementById("quote-" + value.symbol);
-        if (li == null) {
-          li = document.createElement("li");
-          li.id = "quote-" + value.symbol;
-          document.getElementsByClassName("quotes__list")[0].appendChild(li);
-        }
-        li.innerHTML =
-          "<b>" + value.symbol + "</b>" +
-          "</br>bid: " + value.bid +
-          "</br>ask: " + value.ask;
+  renderQuote(): void {
+    (value: Quote) => {
+      let li = document.getElementById("quote-" + value.symbol);
+      if (li == null) {
+        li = document.createElement("li");
+        li.id = "quote-" + value.symbol;
+        document.getElementsByClassName("quotes__list")[0].appendChild(li);
       }
-    );
-  }
+      li.innerHTML =
+        "<b>" + value.symbol + "</b>" +
+        "</br>bid: " + value.bid +
+        "</br>ask: " + value.ask;
+    }
+  };
 
 }
