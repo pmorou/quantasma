7670f9b6723d2fab30c665d8a6bfd64b7eb2702e
Piotr Morończyk <p.moronczyk@gmail.com>
2018-12-11 16:16:24 +0100

    Handle different conditions based on account state value

--- quantasma-app/src/main/resources/frontend/src/app/account-state/account-state.component.ts
+++ quantasma-app/src/main/resources/frontend/src/app/account-state/account-state.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {EventsService} from "../events.service";
+import { EventsService } from "../events.service";
+import { AccountState } from "../../shared/account-state.model";
 
 @Component({
   selector: 'app-account-state',
@@ -8,25 +9,13 @@ import {EventsService} from "../events.service";
 })
 export class AccountStateComponent implements OnInit {
 
-  public equity: number = NaN;
-  public balance: number = NaN;
-  public positionsProfitLoss: number = NaN;
-  public positionsAmount: number = NaN;
-  public usedMargin: number = NaN;
-  public currency: string = "";
-  public leverage: number = NaN;
+  public accountState$?: AccountState;
 
   constructor(private events: EventsService) { }
 
   ngOnInit() {
     this.events.accountState().subscribe(value => {
-      this.equity = value.equity;
-      this.balance = value.balance;
-      this.positionsProfitLoss = value.positionsProfitLoss;
-      this.positionsAmount = value.positionsAmount;
-      this.usedMargin = value.usedMargin;
-      this.currency = value.currency;
-      this.leverage = value.leverage;
+      this.accountState$ = value;
     });
   }
 
