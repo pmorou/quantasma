5d5e4732c03d23d856593fb7f81768b53b875b0e
Piotr Morończyk <p.moronczyk@gmail.com>
2018-12-12 21:02:31 +0100

    Run command on existing file

--- generate_readme.sh
+++ generate_readme.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo 'Converting README-src.adoc to README.md in progress...'
-asciidoctor -b docbook -a leveloffset=+1 -o - README-src.adoc | pandoc --atx-headers --wrap=preserve -t markdown_github -f docbook - > README.md
+if [ -f README-src.adoc ]
+then
+    echo "Converting README-src.adoc to README.md in progress..."
+    asciidoctor -b docbook -a leveloffset=+1 -o - README-src.adoc | pandoc --atx-headers --wrap=preserve -t markdown_github -f docbook - > README.md
+fi
