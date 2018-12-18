f0cde98492115ec41ba883b1bfd6c6d4392ef154
Piotr Morończyk <p.moronczyk@gmail.com>
2018-12-19 00:14:10 +0100

    Test strategy controller

---
+++ quantasma-app/src/test/groovy/quantasma/app/controller/StrategyControllerSpec.groovy
@@ -0,0 +1,34 @@
+package quantasma.app.controller
+
+import org.spockframework.spring.SpringBean
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import quantasma.app.service.StrategyService
+import spock.lang.Specification
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@WebMvcTest(StrategyController)
+class StrategyControllerSpec extends Specification {
+
+    @SpringBean
+    private StrategyService service = Mock()
+
+    @Autowired
+    private MockMvc mockMvc
+
+    def 'status returns OK'() {
+        given:
+        service.all() >> []
+
+        when:
+        mockMvc.perform(get("/strategy/all"))
+                .andExpect(status().isOk())
+
+        then:
+        1 * service.all()
+        noExceptionThrown()
+    }
+}
