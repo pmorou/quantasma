6a9ce5f767ac65dba678f8ba9939e1a7ed6c979b
Piotr Morończyk <p.moronczyk@gmail.com>
2018-12-18 03:56:24 +0100

    Base actions on subjects

--- quantasma-app/src/main/resources/frontend/src/app/dashboard/dashboard.component.ts
+++ quantasma-app/src/main/resources/frontend/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StrategyService } from "../strategy.service";
+import {map, tap} from "rxjs/operators";
+import {flatMap} from "rxjs/operators";
+import {Subject} from "rxjs/index";
 
 @Component({
   selector: 'app-dashboard',
@@ -9,23 +12,40 @@ import { StrategyService } from "../strategy.service";
 export class DashboardComponent implements OnInit {
   strategies$: any = [];
 
+  updateStrategiesSubject: Subject<void> = new Subject();
+  deactivateStrategySubject: Subject<any> = new Subject();
+  activateStrategySubject: Subject<any> = new Subject();
+
   constructor(private strategyService: StrategyService) { }
 
   ngOnInit() {
-    this.getStrategies();
+    this.updateStrategiesSubject.asObservable().pipe(
+      flatMap(() => this.strategyService.getStrategies()),
+      tap((strategies: any[]) => this.strategies$ = strategies))
+    .subscribe();
+
+    this.deactivateStrategySubject.asObservable().pipe(
+      map((id: any) => this.strategyService.deactivate(id)),
+      tap(() => this.updateStrategies))
+    .subscribe();
+
+    this.activateStrategySubject.asObservable().pipe(
+      map((id: any) => this.strategyService.activate(id)),
+      tap(() => this.updateStrategies))
+    .subscribe();
+
+    this.updateStrategies();
   }
 
-  getStrategies() {
-    this.strategyService.getStrategies().subscribe(
-      data => this.strategies$ = data
-    );
+  updateStrategies() {
+    this.updateStrategiesSubject.next();
   }
 
   deactivateStrategy(id: any) {
-    this.strategyService.deactivate(id).subscribe(() => this.getStrategies());
+    this.deactivateStrategySubject.next(id);
   }
 
   activateStrategy(id: any) {
-    this.strategyService.activate(id).subscribe(() => this.getStrategies());
+    this.activateStrategySubject.next(id);
   }
 }
