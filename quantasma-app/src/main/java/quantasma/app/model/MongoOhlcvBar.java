724eed9176fe36d3f848666f079bfbfb24281e66
Piotr Morończyk <p.moronczyk@gmail.com>
2018-12-21 23:51:16 +0100

    Remove annotations
    Indexes are provided in configuration class

--- quantasma-app/src/main/java/quantasma/app/model/MongoOhlcvBar.java
+++ quantasma-app/src/main/java/quantasma/app/model/MongoOhlcvBar.java
@@ -2,8 +2,6 @@ package quantasma.app.model;
 
 import lombok.Data;
 import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import quantasma.core.BarPeriod;
 
 import java.io.Serializable;
@@ -12,13 +10,9 @@ import java.time.Instant;
 @Data
 public class MongoOhlcvBar implements Serializable {
 
-    @Id
     private ObjectId id;
-    @Indexed
     private BarPeriod period;
-    @Indexed(unique = true)
     private Instant date;
-    @Indexed
     private String symbol;
     private double bidOpen;
     private double bidLow;
