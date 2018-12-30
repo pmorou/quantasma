538e55e2ed56ca94efbd6905d1671de35d007fee
Piotr Morończyk <p.moronczyk@gmail.com>
2018-12-30 04:36:37 +0100

    Implement unmodifiable time series

---
+++ quantasma-core/src/main/java/quantasma/core/timeseries/UnmodifiableTimeSeries.java
@@ -0,0 +1,72 @@
+package quantasma.core.timeseries;
+
+import org.ta4j.core.Bar;
+import org.ta4j.core.TimeSeries;
+import org.ta4j.core.num.Num;
+import quantasma.core.timeseries.bar.UnmodifiableBar;
+
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UnmodifiableTimeSeries extends ForwardingTimeSeries {
+    protected UnmodifiableTimeSeries(TimeSeries timeSeries) {
+        super(timeSeries);
+    }
+
+    @Override
+    public void addBar(Bar bar, boolean replace) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addBar(Duration timePeriod, ZonedDateTime endTime) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addBar(ZonedDateTime endTime, Num openPrice, Num highPrice, Num lowPrice, Num closePrice, Num volume, Num amount) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addBar(Duration timePeriod, ZonedDateTime endTime, Num openPrice, Num highPrice, Num lowPrice, Num closePrice, Num volume) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addBar(Duration timePeriod, ZonedDateTime endTime, Num openPrice, Num highPrice, Num lowPrice, Num closePrice, Num volume, Num amount) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addTrade(Num tradeVolume, Num tradePrice) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void addPrice(Num price) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public TimeSeries getSubSeries(int startIndex, int endIndex) {
+        return new UnmodifiableTimeSeries(super.getSubSeries(startIndex, endIndex));
+    }
+
+    @Override
+    public List<Bar> getBarData() {
+        return delegate().getBarData()
+                         .stream()
+                         .map(UnmodifiableBar::new)
+                         .collect(Collectors.collectingAndThen(Collectors.toList(),
+                                                               Collections::unmodifiableList));
+    }
+
+    @Override
+    public Bar getBar(int i) {
+        return new UnmodifiableBar(delegate().getBar(i));
+    }
+}
