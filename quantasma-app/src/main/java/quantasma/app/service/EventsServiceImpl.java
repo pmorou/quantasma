7d4cf794c04f47885e3428059e16a351fa8cf753
Piotr Morończyk <p.moronczyk@gmail.com>
2018-12-31 20:15:40 +0100

    Generic event publishing
    No more creating additional field

--- quantasma-app/src/main/java/quantasma/app/service/EventsServiceImpl.java
+++ quantasma-app/src/main/java/quantasma/app/service/EventsServiceImpl.java
@@ -1,23 +1,25 @@
 package quantasma.app.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import quantasma.app.event.EventBuffer;
 import quantasma.core.TradeEngine;
 import quantasma.integrations.event.AccountStateEvent;
+import quantasma.integrations.event.Event;
 import quantasma.integrations.event.OpenedPositionsEvent;
 import quantasma.integrations.event.QuoteEvent;
 import reactor.core.publisher.Flux;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @Slf4j
 public class EventsServiceImpl implements EventsService {
 
-    private final EventBuffer<QuoteEvent> quoteEventBuffer = EventBuffer.instance(QuoteEvent.class);
-    private final EventBuffer<AccountStateEvent> accountStateEventBuffer = EventBuffer.instance(AccountStateEvent.class);
-    private final EventBuffer<OpenedPositionsEvent> openedPositionsEventBuffer = EventBuffer.instance(OpenedPositionsEvent.class);
-
+    private final Map<Class<?>, EventBuffer<?>> eventBuffers = new HashMap<>();
     private final TradeEngine tradeEngine;
 
     @Autowired
@@ -27,32 +29,40 @@ public class EventsServiceImpl implements EventsService {
 
     @Override
     public Flux<QuoteEvent> quote() {
-        return Flux.from(quoteEventBuffer.publisher());
+        return Flux.from(publisherFor(QuoteEvent.class));
     }
 
     public void publish(QuoteEvent quoteEvent) {
         tradeEngine.process(quoteEvent.data());
-        quoteEventBuffer.setNext(quoteEvent);
+        next(quoteEvent);
     }
 
     @Override
     public Flux<AccountStateEvent> accountState() {
-        return Flux.from(accountStateEventBuffer.publisher());
+        return Flux.from(publisherFor(AccountStateEvent.class));
     }
 
     @Override
     public void publish(AccountStateEvent accountStateEvent) {
-        accountStateEventBuffer.setNext(accountStateEvent);
+        next(accountStateEvent);
     }
 
     @Override
     public Flux<OpenedPositionsEvent> openedPositions() {
-        return Flux.from(openedPositionsEventBuffer.publisher());
+        return Flux.from(publisherFor(OpenedPositionsEvent.class));
     }
 
     @Override
     public void publish(OpenedPositionsEvent openedPositionsEvent) {
-        openedPositionsEventBuffer.setNext(openedPositionsEvent);
+        next(openedPositionsEvent);
     }
 
+    private <E extends Event> Publisher<E> publisherFor(Class<E> clazz) {
+        return (Publisher<E>) eventBuffers.get(clazz).publisher();
+    }
+
+    private <E extends Event> void next(E event) {
+        eventBuffers.putIfAbsent(event.getClass(), EventBuffer.instance(event.getClass()));
+        ((EventBuffer<E>) eventBuffers.get(event.getClass())).setNext(event);
+    }
 }
