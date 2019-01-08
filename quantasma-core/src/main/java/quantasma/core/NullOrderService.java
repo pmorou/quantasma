23dfc246da4a095fac0eca5cf72c9b5748eaec15
Piotr Morończyk <p.moronczyk@gmail.com>
2019-01-08 07:46:24 +0100

    Ignore signal

--- quantasma-core/src/main/java/quantasma/core/NullOrderService.java
+++ quantasma-core/src/main/java/quantasma/core/NullOrderService.java
@@ -6,9 +6,11 @@ import quantasma.core.order.CloseMarkerOrder;
 public class NullOrderService implements OrderService {
     @Override
     public void openPosition(OpenMarketOrder openMarketOrder) {
+        // ignore
     }
 
     @Override
     public void closePosition(CloseMarkerOrder closeMarkerOrder) {
+        // ignore
     }
 }
