a6685a8fc219344786e7667dbb526f8b0fa663d0
Piotr Morończyk <p.moronczyk@gmail.com>
2019-01-10 19:49:22 +0100

    Inject beans
    It will subscribe then automatically

--- quantasma-app/src/main/java/quantasma/app/config/TradeEngineConfig.java
+++ quantasma-app/src/main/java/quantasma/app/config/TradeEngineConfig.java
@@ -1,6 +1,7 @@
 package quantasma.app.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import quantasma.core.BarPeriod;
@@ -44,6 +45,7 @@ public class TradeEngineConfig {
         return BaseTradeEngine.create(context);
     }
 
+    @Autowired
     public void subscribeTradeEngine(EventPublisher eventPublisher, TradeEngine tradeEngine) {
         log.info("Subscribing trade engine to quotes");
         eventPublisher.subscribe(new QuoteEventSubscriber(tradeEngine));
