dfe91d87549c70a614c9e1b31fc96e29c0b5d709
Piotr Morończyk <p.moronczyk@gmail.com>
2019-01-10 13:57:10 +0100

    Extract strategies configuration

--- quantasma-app/src/main/java/quantasma/app/config/IntegrationServiceConfig.java
+++ quantasma-app/src/main/java/quantasma/app/config/IntegrationServiceConfig.java
@@ -5,30 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import quantasma.core.Context;
-import quantasma.core.StrategyControl;
-import quantasma.core.TradeStrategy;
-import quantasma.core.analysis.parametrize.Values;
-import quantasma.examples.RSIStrategy;
 import quantasma.integrations.data.provider.LiveDataProvider;
 import quantasma.integrations.event.EventPublisher;
 
 @Configuration
 @Slf4j
 public class IntegrationServiceConfig {
-
-    @Bean
-    public TradeStrategy rsiStrategy(StrategyControl strategyControl, Context context) {
-        final Values<RSIStrategy.Parameter> parameterValues =
-                Values.of(RSIStrategy.Parameter.class)
-                      .set(RSIStrategy.Parameter.TRADE_SYMBOL, "EURUSD")
-                      .set(RSIStrategy.Parameter.RSI_PERIOD, 14)
-                      .set(RSIStrategy.Parameter.RSI_LOWER_BOUND, 30)
-                      .set(RSIStrategy.Parameter.RSI_UPPER_BOUND, 70);
-        final TradeStrategy strategy = RSIStrategy.buildBullish(context, parameterValues);
-        strategyControl.register(strategy);
-        return strategy;
-    }
 
 
     @Bean
