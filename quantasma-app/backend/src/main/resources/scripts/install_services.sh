2f34349b8cc530db2a6ad33c6af79468306162a1
Piotr Morończyk <p.moronczyk@gmail.com>
2019-02-01 17:43:38 +0100

    Remove scripts
    Replaced with docker-compose

--- quantasma-app/backend/src/main/resources/scripts/install_services.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker run -d \
-  --name historical-mongo-db \
-  -v historical-mongo-db-vol:/app \
-  mongo
--- quantasma-app/backend/src/main/resources/scripts/start_services.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker container start historical-mongo-db
