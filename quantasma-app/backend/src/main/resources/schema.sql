95e6d70e1d11683602a7c1fa05a6dcb48bd8fb2f
Piotr Morończyk <p.moronczyk@gmail.com>
2019-03-24 17:19:06 +0100

    Define foreign keys

--- quantasma-app/backend/src/main/resources/schema.sql
+++ quantasma-app/backend/src/main/resources/schema.sql
@@ -42,7 +42,8 @@ CREATE TABLE orders (
   x_created_on TIMESTAMP NOT NULL,
   x_updated_on TIMESTAMP,
 
-  CONSTRAINT orders_pk_id PRIMARY KEY(id)
+  CONSTRAINT orders_pk_id PRIMARY KEY(id),
+  CONSTRAINT orders_fk_instrument_id FOREIGN KEY (instrument_id) REFERENCES instruments (id)
 );
 
 CREATE TABLE transactions (
@@ -59,7 +60,11 @@ CREATE TABLE transactions (
   x_created_on TIMESTAMP NOT NULL,
   x_updated_on TIMESTAMP,
 
-  CONSTRAINT transactions_pk_id PRIMARY KEY(id)
+  CONSTRAINT transactions_pk_id PRIMARY KEY(id),
+  CONSTRAINT transactions_fk_broker_id FOREIGN KEY (broker_id) REFERENCES brokers (id),
+  CONSTRAINT transactions_fk_open_order_id FOREIGN KEY (open_order_id) REFERENCES orders (id),
+  CONSTRAINT transactions_fk_close_order_id FOREIGN KEY (close_order_id) REFERENCES orders (id),
+  CONSTRAINT transactions_fk_strategy_id FOREIGN KEY (strategy_id) REFERENCES strategies (id)
 );
 
 CREATE TABLE brokers (
