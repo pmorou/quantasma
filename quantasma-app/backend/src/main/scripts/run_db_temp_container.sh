9870fc0f9ce857d671326acfbbde55dffc996159
Piotr Morończyk <p.moronczyk@gmail.com>
2019-03-31 15:22:11 +0200

    Move sql scripts to migration dir

--- quantasma-app/backend/src/main/scripts/run_db_temp_container.sh
+++ quantasma-app/backend/src/main/scripts/run_db_temp_container.sh
@@ -2,4 +2,4 @@
 
 echo 'Running docker container...'
 
-sudo docker run --name jooq-gen-container -p 5432:5432 -v "$PWD"/quantasma-app/backend/src/main/resources/sql:/docker-entrypoint-initdb.d -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -d postgres:11.2
+sudo docker run --name jooq-gen-container -p 5432:5432 -v "$PWD"/quantasma-app/backend/src/main/resources/db/migration:/docker-entrypoint-initdb.d -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -d postgres:11.2
