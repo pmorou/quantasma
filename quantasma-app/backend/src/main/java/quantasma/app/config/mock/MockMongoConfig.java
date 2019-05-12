cfab35fdc1b8f973873dccf7cc4c80d749f44caa
Piotr Morończyk <p.moronczyk@gmail.com>
2019-05-12 15:22:18 +0200

    Provide javadoc behind MockMongoConfig

--- quantasma-app/backend/src/main/java/quantasma/app/config/mock/MockMongoConfig.java
+++ quantasma-app/backend/src/main/java/quantasma/app/config/mock/MockMongoConfig.java
@@ -9,6 +9,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+/**
+ * Mongo configuration for in-memory DB.
+ *
+ * Provides {@code MongoTemplate} for tests.
+ */
 @Configuration
 @Profile("mock")
 @EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
